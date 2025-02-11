166d56c91622b6b9436e61655323438f474cfdfa
DenisGalandSAP <denisgaland@gmail.com>
2025-02-11 16:31:49 +0100

    0.0.2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox"
+
 ],
     function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("nmc.be.project5.controller.View1", {
             onInit: function () {
-                MessageBox.confirm("Hello World");
+
+            },
+
+            onSelection: function (oEvent) {
+                var oItem = oEvent.getParameter('listItem');
+                var oContext = oItem.getBindingContext();
+                var sName = oContext.getProperty('Name');
+                MessageBox.show(sName);
             }
         });
     });
