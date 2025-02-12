833449055605f947c287c7d6aa835b93067e7b27
DenisGalandSAP <denisgaland@gmail.com>
2025-02-12 12:27:57 +0100

    0.0.3

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
+    "../controller/BaseController",
     "sap/m/MessageBox"
 
 ],
@@ -7,15 +7,53 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("nmc.be.project5.controller.View1", {
+
             onInit: function () {
 
             },
 
             onSelection: function (oEvent) {
+                this.getModel('View1').setProperty('/UDButtonVisible', true);
                 var oItem = oEvent.getParameter('listItem');
-                var oContext = oItem.getBindingContext();
-                var sName = oContext.getProperty('Name');
-                MessageBox.show(sName);
+                this.oContext = oItem.getBindingContext();
+                this.sPath = this.oContext.getPath();
+            },
+
+            onCreate: function () {
+
+            },
+
+            onUpdate: async function () {
+                if (!this.oEditFragment) {
+                    this.oEditFragment = await this.loadFragment({ name: 'nmc.be.project5.view.fragment.edit' });
+                    this.getView().addDependent(this.oEditFragment);
+                }
+                this.oEditFragment.bindElement(this.sPath);
+                this.oEditFragment.open();
+            },
+
+            onCancelEdit: function () {
+                this.oEditFragment.close();
+            },
+
+            onSaveEdit: function () {
+                this.getModel().update(this.sPath, this.oContext.getObject(), {
+                    success: (oResponse) => { this.oEditFragment.close() },
+                    error: (oError) => { }
+                });
+            },
+
+            onDelete: function () {
+                MessageBox.confirm(this.getResourceBundle().getText('confirmDelete'), {
+                    onClose: (oAction) => {
+                        if (oAction === 'OK') {
+                            this.getModel().remove(this.sPath, {
+                                success: (oResponse) => { this.getModel('View1').setProperty('/UDButtonVisible', false); },
+                                error: (oError) => { }
+                            })
+                        }
+                    }
+                });
             }
         });
     });
